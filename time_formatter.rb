16f5a9234ac0d320352554cb806f086a4c7553fa
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-10-01 00:21:15 +0300

    Update time_formatter.rb

--- time_formatter.rb
+++ time_formatter.rb
@@ -1,39 +1,61 @@
 class TimeFormatter
 
-  def call(query)
-    @time = Time.now
-    return format_date(query)
+  VALID_WORDS = ["year", "month", "day", "hour", "minute", "second"]
+
+  attr_accessor :invalid_words, :valid_words
+
+  def initialize
+    @invalid_words = [ ]
+    @valid_words = [ ]
   end
 
-  def create_hash_with_time_formats(query)
-    @time_formats = { }
-    query.each do |word|
-      case word.to_sym
-      when :year
-        @time_formats[:year] ="%Y"
-      when :month
-        @time_formats[:month] ="%b"
-      when :day
-        @time_formats[:day] = "%d"
-      when :hour 
-       @time_formats[:hour ] ="%H"
-      when :minute
-        @time_formats[:minute] ="%M"
-      when :second
-         @time_formats[:second] ="%S"
+  def query_valid?(query)
+    query = get_query_from_request(query)
+    if query.empty? 
+      return false
+    else
+      get_valid_and_invalid_words(query)
+      return true if @invalid_words.empty?
+    end
+  end
+
+  def get_valid_and_invalid_words(query)
+    query.each do |query_word|
+      if VALID_WORDS.include?(query_word)
+        @valid_words << query_word
+      else  
+        @invalid_words << query_word
+      end 
+    end
+  end
+
+  def get_query_from_request(query)
+    request_query = Rack::Utils.parse_nested_query(query).values
+    request_query=request_query[0].split(",")
+  end
+
+  def format_date
+    Time.now.strftime(string_for_date_conversion.join(" "))
+  end
+
+  def string_for_date_conversion
+    string_for_date_conversion = [ ]
+    @valid_words.each do |word|
+      case word
+      when "year"
+        string_for_date_conversion << "%Y"
+      when "month"
+        string_for_date_conversion << "%b"
+      when "day"
+        string_for_date_conversion << "%d"
+      when "hour" 
+       string_for_date_conversion << "%H"
+      when "minute"
+        string_for_date_conversion << "%M"
+      when "second"
+         string_for_date_conversion << "%S"
       end
     end
+    return string_for_date_conversion
   end
-
-  def format_date(query)
-    body = [ ]
-    formated_time = " "
-    create_hash_with_time_formats(query)
-    query.each do |word|
-      formated_time= formated_time + " " + "#{@time.strftime(@time_formats[word.to_sym])}"
-    end
-    body << formated_time
-    return body
-  end
-
   end
