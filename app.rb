a27ba4a43d3930f447dec34692a283f258fe2dc4
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-10-02 00:05:32 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,12 +5,8 @@ class App
   def call(env)
     request = Rack::Request.new(env) 
     if request_correct?(request)
-      time_formatter = TimeFormatter.new
-        if time_formatter.query_valid?(request.query_string)
-          create_response(200, time_formatter.format_date)
-        else 
-         create_response(400, ["Unknown time format: #{time_formatter.invalid_words.join(" ")}"])
-        end
+      time_formatter = TimeFormatter.new(request.query_string)
+      create_response_to_valid_or_invalid_requests(time_formatter, request)
     else
       create_response(404, ["Page not found"])
     end     
@@ -25,6 +21,14 @@ private
   def create_response(status, body)
     response = Rack::Response.new(body, status, {'Content-Type' => 'text/plain'})
     response.finish
+  end
+
+  def create_response_to_valid_or_invalid_requests(time_formatter, request)
+    if time_formatter.query_valid?(request.query_string)
+      create_response(200, time_formatter.format_date)
+    else 
+      create_response(400, ["Unknown time format: #{time_formatter.invalid_words.join(" ")}"])
+    end
   end
   
 end
