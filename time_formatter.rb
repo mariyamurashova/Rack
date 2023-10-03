04cd8819ebe9f1be7fe59b7ac8ebc6806f74d820
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-10-03 21:25:31 +0300

    Update time_formatter.rb

--- time_formatter.rb
+++ time_formatter.rb
@@ -1,6 +1,5 @@
 class TimeFormatter
 
-  VALID_WORDS = ["year", "month", "day", "hour", "minute", "second"]
   DATE_FORMATTER = {"year" => "%Y", "month" =>"%b", "day" =>"%d", "hour" => "%H", "minute" => "%M", "second" => "%S" }
 
   attr_accessor :invalid_words, :valid_words, :query
@@ -8,16 +7,11 @@ class TimeFormatter
   def initialize(query)
     @invalid_words = [ ]
     @valid_words = [ ]
-    @query = Rack::Utils.parse_nested_query(query).values
+    @query = (Rack::Utils.parse_nested_query(query).values)[0].split(",")
   end
 
   def call
-    converted_request_query
     get_valid_and_invalid_words
-  end
-
-  def converted_request_query
-    @query = @query[0].split(",")
   end
 
   def query_valid?
@@ -26,7 +20,7 @@ class TimeFormatter
 
   def get_valid_and_invalid_words
     @query.each do |query_word|
-      if VALID_WORDS.include?(query_word)
+      if DATE_FORMATTER.keys.include?(query_word)
         @valid_words << DATE_FORMATTER[query_word]
       else  
         @invalid_words << query_word
